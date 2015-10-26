2794824b41f030d31e75dcebdc2a4a775d7fefb8
t-souther <tarvis-souther@hotmail.com>
2015-10-26 10:25:54 -0500

    first commit

---
+++ src/howtodocumentaproject/funwithfuntions.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package howtodocumentaproject;
+
+/**
+ * @author Travis
+ *
+ */
+public class funwithfuntions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
